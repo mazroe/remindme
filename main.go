fed869e3739915b1b2734ea533caf9d7a87ec508
mazroe <khalid.almazrouai@gmail.com>
2018-08-11 11:44:02 +0400

    init for db

--- main.go
+++ main.go
@@ -5,11 +5,18 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/mazroe/remindme/db"
+
 	"github.com/gin-gonic/gin"
 	_ "github.com/heroku/x/hmetrics/onload"
 )
 
 func main() {
+	db.Save("the world is my country", "MazroE")
+	db.Save("Humanity is my race", "MazroE")
+	db.Save("Human history is my shame and pride", "MazroE")
+	qs, _ := db.All()
+
 	port := os.Getenv("PORT")
 
 	if port == "" {
@@ -22,7 +29,7 @@ func main() {
 	router.Static("/static", "static")
 
 	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl.html", nil)
+		c.HTML(http.StatusOK, "index.tmpl.html", gin.H{"qoutes": qs})
 	})
 
 	router.Run(":" + port)
